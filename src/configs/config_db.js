9c895492a9931641bcf5a616f872af9251541183
Adán Lijó <a19adanlg@iessanclemente.net>
2022-11-07 23:17:30 +0100

    API

--- src/configs/config_db.js
+++ src/configs/config_db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_CNN);
+        await mongoose.connect(process.env.MONGODB_CNN, { useNewUrlParser: true, useUnifiedTopology: true });
         return 'Conexión establecida con la database';
     } catch (error) {
         console.log(error)
